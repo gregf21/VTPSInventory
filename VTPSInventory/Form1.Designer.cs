989b36b64c80e8ab4c0fdbb550222a7d8a5393a4
gregf21 <gregory12006@yahoo.com>
2019-12-12 16:27:55 -0500

    First Commit

--- VTPSInventory/Form1.Designer.cs
+++ VTPSInventory/Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.itemTextbox = new System.Windows.Forms.TextBox();
+            this.itemSearchLabel = new System.Windows.Forms.Label();
+            this.itemNameLabel = new System.Windows.Forms.Label();
+            this.itemLocationLabel = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // itemTextbox
+            // 
+            this.itemTextbox.Location = new System.Drawing.Point(19, 49);
+            this.itemTextbox.Name = "itemTextbox";
+            this.itemTextbox.Size = new System.Drawing.Size(201, 26);
+            this.itemTextbox.TabIndex = 0;
+            // 
+            // itemSearchLabel
+            // 
+            this.itemSearchLabel.AutoSize = true;
+            this.itemSearchLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.itemSearchLabel.Location = new System.Drawing.Point(12, 9);
+            this.itemSearchLabel.Name = "itemSearchLabel";
+            this.itemSearchLabel.Size = new System.Drawing.Size(208, 37);
+            this.itemSearchLabel.TabIndex = 1;
+            this.itemSearchLabel.Text = "Item to Find:";
+            // 
+            // itemNameLabel
+            // 
+            this.itemNameLabel.AutoSize = true;
+            this.itemNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.itemNameLabel.Location = new System.Drawing.Point(12, 128);
+            this.itemNameLabel.Name = "itemNameLabel";
+            this.itemNameLabel.Size = new System.Drawing.Size(191, 37);
+            this.itemNameLabel.TabIndex = 2;
+            this.itemNameLabel.Text = "Item Name: ";
+            // 
+            // itemLocationLabel
+            // 
+            this.itemLocationLabel.AutoSize = true;
+            this.itemLocationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.itemLocationLabel.Location = new System.Drawing.Point(12, 165);
+            this.itemLocationLabel.Name = "itemLocationLabel";
+            this.itemLocationLabel.Size = new System.Drawing.Size(227, 37);
+            this.itemLocationLabel.TabIndex = 3;
+            this.itemLocationLabel.Text = "Item Location: ";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(19, 81);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(201, 44);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(456, 232);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.itemLocationLabel);
+            this.Controls.Add(this.itemNameLabel);
+            this.Controls.Add(this.itemSearchLabel);
+            this.Controls.Add(this.itemTextbox);
+            this.Name = "Form1";
+            this.Text = "Inventory Search";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox itemTextbox;
+        private System.Windows.Forms.Label itemSearchLabel;
+        private System.Windows.Forms.Label itemNameLabel;
+        private System.Windows.Forms.Label itemLocationLabel;
+        private System.Windows.Forms.Button searchButton;
     }
 }
 
--- VTPSInventory/Form1.cs
+++ VTPSInventory/Form1.cs
@@ -7,14 +7,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.Threading;
 
 namespace VTPSInventory
 {
     public partial class Form1 : Form
     {
+        static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static readonly string ApplicationName = "InventoryLocations";
+        static readonly string SpreadsheetId = "14LTmOA1xTyFOZ0eHHOENEZzabqh5bnTXVgC6Zs_QZj4";
+        static readonly string sheet = "InventoryLocations";
+        static SheetsService service;
+          
         public Form1()
         {
             InitializeComponent();
+            GoogleCredential credential;
+            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream)
+                    .CreateScoped(Scopes);
+            }
+
+            // Create Google Sheets API service.
+            service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void ReadEntries()
+        {
+            var range = $"{sheet}!A:B";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    service.Spreadsheets.Values.Get(SpreadsheetId, range);
+
+            var response = request.Execute();
+            IList<IList<object>> values = response.Values;
+            if (values != null && values.Count > 0)
+            {
+                foreach (var row in values)
+                {
+                    if (row[0].ToString().Contains(itemTextbox.Text))
+                    {
+                        itemNameLabel.Text = "Item Name: " + row[0];
+                        itemLocationLabel.Text = "Item Location: " + row[1];
+                    }
+                }
+            }
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            ReadEntries();
         }
     }
 }
